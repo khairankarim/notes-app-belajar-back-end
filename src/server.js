d759b5340c109339a68be61dd780413be1f19213
khairankarim <muhammadkhairankarim@gmail.com>
2026-03-10 15:45:26 +0800

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import routes from './routes.js';
 import cors from 'cors';
- 
-const app = express();
-const port = 3000;
-const host = 'localhost';
 
 // const app = express();
-// const port = process.env.PORT || 3000;
-// const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
+// const port = 3000;
+// const host = 'localhost';
+
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 
 app.use(cors({
   origin: '*'
